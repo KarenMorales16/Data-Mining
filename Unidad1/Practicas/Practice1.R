b4a7ceab8ab80ae91de7b669f6b01a8f02dd2689
KarenMorales16 <karen.morales16@tectijuana.edu.mx>
2020-07-09 19:55:39 -0700

    Readme

---
+++ Unidad1/Practicas/Practice1.R
@@ -0,0 +1,32 @@
+#Test the Law Of Large Numbers for N random normally distributed numbers with mean = 0, stdev=1:
+  
+#Create an R script that will count how many of these numbers fall between -1 and 1 and divide by the total quantity of N
+
+#You know that E(X) = 68.2%
+#Check that Mean(Xn)->E(X) as you rerun your script while increasing N
+
+#Hint:
+#1. Initialize sample size
+n<-10000
+
+#2. Initialize counter
+  contador <-0
+  resultado <-0
+  
+#3. loop for(i in rnorm(size))
+for(i in 1:n)
+{
+  Norm <- rnorm(i, mean = 0, sd = 1)
+  
+#4. Compruebe si la variable iterada cae
+  if(Norm >=-1 && Norm  <=1){
+    #5. Aumente el contador si la condición es verdadera
+    contador<- contador+1
+ 
+  }
+}
+
+#6. devolver un resultado <- contador / N
+resultado <-contador/n
+print(contador)
+print(resultado) 
---
+++ Unidad1/Practicas/Practice2.R
@@ -0,0 +1,75 @@
+# Practice find 20 more funtions in R and make an example of it.
+
+#1
+?names()
+
+x <- c(2,4,6)  #Assign 3 values to the variable x
+names(x) <- c("I","II","III")  # Assigns names to the previous 3 values
+x
+
+#2
+cat() # This function writes text and variables in the output.
+
+x <- 2
+y <- 4
+cat(x,"elevado a",y,"es",x ^ y,"\n")
+
+#3
+invisible () #Hide x in the return function so that the result is not visible.
+
+suma <- function(a, b) {
+  s <- a + b
+  return(invisible(s))
+}
+suma(5,4)
+
+#4
+length () # Shows the number of elements in a vector
+
+x <- 1:20 # The numbers from 1 to 20 are saved in x
+length (x) # Number of elements of x
+
+#5
+trunc () # Remove decimals from a number
+
+trunc (1.999999)
+
+#6
+round (x, decimals = 0) # Round a number with decimals
+
+round (6.78) # By not indicating decimals the result is an approximate integer
+
+round (6.78.1) # It is rounded with a decimal
+
+#7
+sum (x) #Add all the elements of a vector x
+
+x <- runif (100,1,10) # 100 random numbers are generated between 1 and 10 and saved in the variable x
+sum (x) # Sum of the 100 random numbers
+
+#8
+edad <- function() {
+  r <- readline("Escribe tu edad : ")
+  r <- as.integer(r)  # Convierte r en un entero. Si se desean decimales usar as.double()
+  rmas1 <- r + 1
+  cat("Dentro de un año tendrás",rmas1,"años\n")
+  return(invisible(rmas1))
+}
+
+edad()
+
+#9
+any () #Returns T if any element meets the condition Example:
+
+x <- runif (10, -10, 100) # 10 random numbers between -10 and 100 are generated
+if (any (x <0)) cat ("In x there are negative numbers \ n") # If any numbers of x are negative
+
+#10 
+coef() #Coef is a generic function which extracts model coefficients from objects returned by modeling functions.
+
+x <- 1:5; coef(lm(c(1:3, 7, 6) ~ x))
+
+#11
+hist(rnorm(200)) # Generate a histogram of the 200 random numbers with normal distribution
+
+
